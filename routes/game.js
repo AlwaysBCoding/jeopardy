81bf917c2fd0dc85d3208e67e87e7ac75bb2afd1
Jordan Leigh <AlwaysBCoding@gmail.com>
2013-10-28 00:29:48 -0400

    Can write and read the data from the UI, getting closer to setting up a game

--- routes/game.js
+++ routes/game.js
@@ -5,7 +5,7 @@ exports.main = function(request, response) {
   var client = new pg.Client(connectionString);
   client.connect();
 
-  var categories = {};
+  var data = { "categories": {} };
   var category_ids = [];
 
   var formatSet = function(array) {
@@ -14,16 +14,19 @@ exports.main = function(request, response) {
 
   query1 = client.query("SELECT ct.id, ct.name, EXTRACT(YEAR FROM ct.game_date)  FROM categories ct ORDER BY RANDOM() LIMIT 6", function(error, result) {
     _.each(result.rows, function(row, index) {
-      categories[index] = row;
+      data["categories"][row.id] = row;
+      data["categories"][row.id]["clues"] = [];
     });
-    category_ids = _.map(categories, function(x) { return x.id });
+    category_ids = _.map(data["categories"], function(x) { return x.id });
 
-    query2 = client.query("SELECT c.* FROM clues c WHERE c.category_id IN " + formatSet(category_ids), function(error, result) {
-      categories["clues"] = result.rows;
+    query2 = client.query("SELECT c.question, c.answer, c.value, c.category_id FROM clues c WHERE c.category_id IN " + formatSet(category_ids), function(error, result) {
+      _.each(result.rows, function(row, index) {
+        data["categories"][row.category_id]["clues"].push(row);
+      });
     });
 
     query2.on("end", function() {
-      response.render("game/main", {title: "Jeopardy", result: categories, ids: category_ids});
+      response.render("game/main", {title: "Jeopardy", result: data, ids: category_ids});
     });
   });
 
