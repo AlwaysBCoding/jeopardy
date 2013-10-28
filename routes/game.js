7eb678bc3792570b847ba607493005c452fae073
Jordan Leigh <AlwaysBCoding@gmail.com>
2013-10-27 23:40:42 -0400

    Figured out how to administer multiple queries

--- routes/game.js
+++ routes/game.js
@@ -1,14 +1,30 @@
 exports.main = function(request, response) {
+  var _  = require("underscore")._;
   var pg = require("pg");
   var connectionString = process.env.DATABASE_URL || "postgres://localhost:5432/jeopardy_development";
   var client = new pg.Client(connectionString);
   client.connect();
 
-  var testCount;
+  var categories = {};
+  var category_ids = [];
 
-  client.query("SELECT COUNT(*) FROM clues", function(error, result) {
-    testCount = result.rows[0].count;
-    response.render("game/main", {title: "Jeopardy", count: testCount});
+  var formatSet = function(array) {
+    return "(" + array.toString() + ")"
+  }
+
+  query1 = client.query("SELECT ct.id, ct.name, EXTRACT(YEAR FROM ct.game_date)  FROM categories ct ORDER BY RANDOM() LIMIT 6", function(error, result) {
+    _.each(result.rows, function(row, index) {
+      categories[index] = row;
+    });
+    category_ids = _.map(categories, function(x) { return x.id });
+
+    query2 = client.query("SELECT c.* FROM clues c WHERE c.category_id IN " + formatSet(category_ids), function(error, result) {
+      categories["clues"] = result.rows;
+    });
+
+    query2.on("end", function() {
+      response.render("game/main", {title: "Jeopardy", result: categories, ids: category_ids});
+    });
   });
 
 }
